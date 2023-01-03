4482f3eed4339abc50875f69d096049f273d20b4
santiago <murdok@pop-os.localdomain>
2023-01-03 15:22:44 -0500

    update page to deploy versel check and test

--- src/views/cart.tsx
+++ src/views/cart.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
 import CardMovie from '../components/cardMovie'
 import { MovieContext } from '../context/MovieContext'
 import { CartSection } from '../styles/Cart.style'
